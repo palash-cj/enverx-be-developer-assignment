d70dd63472048a268610cb3156676d6844baa074
Palash Jamaiwar <palash.vdoit@gmail.com>
2023-07-28 13:49:11 +0530

    Did changes to receive formdata

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const app=express.Router();
+const multer=require("multer");
+const upload=multer();
 const {feed, getPost, createPost, updatePost, deletePost}=require('../controllers/postController');
 
 app.get('/',feed);
 app.get('/:id',getPost);
-app.post('/',createPost);
-app.put('/:id',updatePost);
+app.post('/',upload.none(),createPost);
+app.put('/:id',upload.none(),updatePost);
 app.delete('/:id',deletePost);
 
 module.exports=app;
